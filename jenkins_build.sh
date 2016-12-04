227ebfc08c6ea4698c06fc6dd985b93efdb43fa2
FrankFan <frankfancode@gmail.com>
2016-12-04 13:30:56 +0800

    add jenkins build script

---
+++ jenkins_build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+build_number=$1
+
+if [ -z $build_number ]; then
+	build_number=0
+fi
+
+build_type=$2
+
+if [ -z $build_type ]; then
+	build_type="Test"
+fi
+
+
+root_path="`dirname $0`"
+root_path_prefix=${root_path:0:1}
+if [[ "$root_path_prefix" != "/" ]]; then
+	root_path="`pwd`/${root_path}"
+fi
+
+if [[ ! -z "$build_number" ]]  && [[ ! -z "$root_path" ]]; then
+		rm -rf ${root_path}/output/Build_${build_number}
+fi
+
+mkdir -p ${root_path}/output/Build_${build_number}
+cp ${root_path}/comment.txt ${root_path}/output/Build_${build_number}/comment.txt
+
+if [[ "$build_type" = "Test" ]]; then
+	${root_path}/gradlew --no-daemon clean assembleDemo官网Qa -PBUILD_NUMBER=${build_number} || exit -1
+	${root_path}/gradlew --no-daemon clean assembleOnline官网Qa -PBUILD_NUMBER=${build_number} || exit -1
+else
+	${root_path}/gradlew --no-daemon clean assembleRelease -PBUILD_NUMBER=${build_number} || exit -1
+fi
